f3476b3d5be481bd4d17052894801ddf86d6df71
Geriano <gerznewbie@gmail.com>
2023-12-09 00:23:20 +0700

    feat(main): update common version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use std::{env, usize::MAX};
-
-use actix_web::{HttpServer, App, web::{PayloadConfig, JsonConfig, FormConfig, self, Data}};
-use nightmare_common::{database::{DB, self}, log};
+use actix_web::web;
 
 #[macro_use] extern crate actix_web;
 
@@ -18,21 +15,8 @@ mod test;
 
 #[actix::main]
 async fn main() -> Result<(), std::io::Error> {
-    let db = DB {
-        auth: database::connect(env::var("AUTH_URL").unwrap()).await,
-        main: database::connect(env::var("DATABASE_URL").unwrap()).await,
-    };
-
-    let auth = db.auth.ping().await;
-    let main = db.main.ping().await;
-
-    let server = HttpServer::new(move || {
-        App::new()
-            .app_data(PayloadConfig::new(MAX))
-            .app_data(JsonConfig::default().limit(MAX))
-            .app_data(FormConfig::default().limit(MAX))
-            .app_data(Data::new(db.clone()))
-            .app_data(Data::new(db.main.clone()))
+    nightmare_common::app::serve(|| {
+        web::scope("")
             .service(api::service())
             .service(
                 web::scope("/api/v1")
@@ -42,14 +26,5 @@ async fn main() -> Result<(), std::io::Error> {
                     .service(controllers::storage::show)
                     .service(controllers::storage::delete)
             )
-    })
-        .workers(4)
-        .bind(("0.0.0.0", 8000))?
-        .run();
-
-    log::info!(main, "Server started at http://localhost:8080",);
-    log::info!(main, "Auth database status {:?}", auth);
-    log::info!(main, "Auth database status {:?}", main);
-
-    server.await
+    }).await
 }
